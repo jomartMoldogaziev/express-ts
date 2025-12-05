966ab4f054f75b158bc793a935463550e5f8b171
Zhoomart <zhoomart@gmail.com>
2025-12-05 23:57:46 +0600

    update-2

--- src/modules/todo/todo.controller.ts
+++ src/modules/todo/todo.controller.ts
@@ -1,17 +1,17 @@
 import { Request, Response } from "express";
 
 interface TodoType {
+    id: number
   name: string;
   description: string;
 }
 
+   const data: TodoType[] = [];
+
 const getTodos = async (req: Request, res: Response) => {
 
 try {
-    const data: TodoType[] = [
-        {name: "Sample Todo",
-        description: "This is a sample todo item."}
-    ];
+ 
     res.status(200).send({
         success: true,
         data: data,
@@ -20,4 +20,90 @@ try {
     console.error("Error in getTodos:", e);
   }  
 };
-export default { getTodos };
+
+const createTodo = async (req: Request, res: Response) => {
+    const { name, description } = req.body as TodoType;
+    try {
+        const newData = {
+            id: data.length + 1,
+            name,
+            description,    
+        }
+
+        data.push(newData);
+
+        res.status(201).send({
+            success: true,
+            data: newData,
+        });
+    } catch (e) {
+        console.error("Error in createTodo:", e);
+    }   
+}
+ 
+const deleteTodo = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const todoIndex = data.findIndex(todo => todo.id === +id!);
+
+        data.splice(todoIndex, 1);
+
+        res.status(200).send({
+            success: true,
+            data: todoIndex, 
+        });
+    } catch (e) {
+        console.error("Error in deleteTodo:", e);
+    }
+};
+ 
+const updateTodo = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { name, description } = req.body as TodoType;
+    try {
+        const todoIndex = data.findIndex(todo => todo.id === +id!);
+        if (todoIndex === -1) {
+            return res.status(404).send({
+                success: false,
+                message: "Todo not found",
+            });
+        }   
+        data[todoIndex] = {
+            id: +id!,
+            name,
+            description,
+        };  
+        res.status(200).send({
+            success: true,
+            data: data[todoIndex],
+        });
+    } catch (e) {
+        console.error("Error in updateTodo:", e);
+    }
+};
+
+const patchTodo = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { name, description } = req.body;
+    try {
+        const todoIndex = data.findIndex(todo => todo.id === +id!);
+        if (todoIndex === -1) {
+            return res.status(404).send({
+                success: false,
+                message: "Todo not found",
+            });
+        }
+        if (name !== undefined) data[todoIndex]!.name = name;
+        if (description !== undefined) data[todoIndex]!.description = description;
+
+
+        res.status(200).send({
+            success: true,
+            data: data[todoIndex],
+        });
+    } catch (e) {
+        console.error("Error in patchTodo:", e);
+    }   
+};
+
+export default { getTodos, createTodo, deleteTodo, updateTodo, patchTodo };
--- src/modules/todo/todo.routes.ts
+++ src/modules/todo/todo.routes.ts
@@ -4,5 +4,11 @@ import todoContrloller from "./todo.controller";
 const router = Router();
 
 router.get("/get-all", todoContrloller.getTodos);
+router.post("/create", todoContrloller.createTodo);
+router.delete("/delete/:id", todoContrloller.deleteTodo);
+router.put("/update/:id", todoContrloller.updateTodo);
+router.patch("/update-part/:id", todoContrloller.patchTodo);
+
+
 
 export default router;
